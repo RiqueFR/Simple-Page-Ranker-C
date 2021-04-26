0f1dcc01bcff3c5c8c6bb48a69de7940ea49b188
HenriqueCruz <henriquepaulinocruz@hotmail.com>
2021-04-25 23:41:34 -0300

    Started the PR calc and the words search

---
+++ pagerank.h
@@ -0,0 +1,9 @@
+#ifndef PAGERANK_H_
+#define PAGERANK_H_
+
+#include "graph.h"
+#include "page.h"
+
+void calc_PR(Graph* graph, Page** pages);
+
+#endif
---
+++ trie/trie.h
@@ -0,0 +1,16 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+typedef struct trie Trie;
+
+Trie* getNewTrieNode();
+
+void insert(Trie* head, char* str);
+
+int search(Trie* head, char* str);
+
+int hasChildren(Trie* curr);
+
+int deletion(Trie** curr, char* str);
+
+#endif
